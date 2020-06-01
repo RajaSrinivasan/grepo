db105f3e2cefbf2274cd483d618005f5caaf0151
Srini <s@srin.me>
2020-06-01 16:15:15 -0400

    initial tagged release

--- impl/version/version.go
+++ impl/version/version.go
@@ -1,12 +1,12 @@
 package version
 // Go package generator
 // File: version.h
-const buildTime = "Sat May 30 2020 06:37:42"
+const buildTime = "Mon Jun 1 2020 16:14:48"
 const versionMajor = 0
 const versionMinor = 1
 const versionBuild = 0
 const repoURL = "https://github.com/RajaSrinivasan/grepo.git"
 const branchName = "master"
-const shortCommitId = "a6816da"
-const longCommitId = "a6816da4b98fd7f0e54f0424016f1ff5d7bfd82b"
-const assignedTags = ""
+const shortCommitId = "91567d1"
+const longCommitId = "91567d16991d74fad51aab52e83ba8aa3597a687"
+const assignedTags = "v0.1,"
